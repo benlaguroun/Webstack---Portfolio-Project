032438815f15667d69450f79f73b6736f3fb5deb
benlaguroun <benlaguroun@gmail.com>
2024-05-18 18:17:04 +0100

    product oage

--- pet-care-ecommerce/pet-care/src/pages/Products.js
+++ pet-care-ecommerce/pet-care/src/pages/Products.js
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { getProducts } from '../services/productService';
+import React from 'react';
+import products from '../data/products';
 import ProductCard from '../components/ProductCard';
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const products = await getProducts();
-      setProducts(products);
-    };
-
-    fetchProducts();
-  }, []);
-
   return (
-    <div>
+    <div className="products-page">
       <h1>Products</h1>
       <div className="product-list">
-        {products.map(product => (
+        {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
@@ -27,3 +16,4 @@ const Products = () => {
 };
 
 export default Products;
+
