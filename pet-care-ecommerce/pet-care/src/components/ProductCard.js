a6743fb1ab2922399d91557191a712f3622dea5d
benlaguroun <benlaguroun@gmail.com>
2024-05-18 18:43:46 +0100

    productcard component to use the addtocard function from the cartcontext

--- pet-care-ecommerce/pet-care/src/components/ProductCard.js
+++ pet-care-ecommerce/pet-care/src/components/ProductCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './ProductCard.css';
+import { useCart } from '../context/CartContext';
 
 const ProductCard = ({ product }) => {
+  const { addToCart } = useCart();
+
   return (
     <div className="product-card">
       <img src={product.image} alt={product.name} />
       <h2>{product.name}</h2>
       <p>{product.description}</p>
       <p>${product.price.toFixed(2)}</p>
-      <button>Add to Cart</button>
+      <button onClick={() => addToCart(product)}>Add to Cart</button>
     </div>
   );
 };
