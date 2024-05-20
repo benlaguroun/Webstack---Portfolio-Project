3318eaf7ae237fdb8d6f834f224e16369b802b92
benlaguroun <benlaguroun@gmail.com>
2024-05-20 11:21:17 +0100

    add logo to header

--- pet-care-ecommerce/pet-care/src/components/Header.js
+++ pet-care-ecommerce/pet-care/src/components/Header.js
@@ -1,19 +1,33 @@
-// Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-const Header = () => (
-  <header>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
-      <Link to="/cart">Cart</Link>
-      <Link to="/order-history">Order History</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header>
+      <div className="logo">
+        <img src="/path/to/logo.png" alt="Logo" />
+      </div>
+      <input type="checkbox" id="menu-toggle" checked={menuOpen} onChange={toggleMenu} />
+      <label htmlFor="menu-toggle" className="menu-icon" onClick={toggleMenu}>
+        <span className="navicon"></span>
+      </label>
+      <nav className={menuOpen ? 'open' : ''}>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/cart">Cart</Link>
+        <Link to="/order-history">Order History</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
